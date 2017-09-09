ab8169a131481c1a38767d3db4c60fe053df3572
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-09 13:29:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var katzDeli = []
 
 function currentLine (line){
-  if (!line.length);
+  if (!line.length){
   return "The line is currently empty."
 }
+
+  const numberAndNames = []
+
+  for (let i=0, l=line.length; i<l; i++){
+    numberAndNames.push (`${i+1}. ${line[i]}`)
+  }
+  return `The line is currently: ${numberAndNames.join(, )}`
+};
