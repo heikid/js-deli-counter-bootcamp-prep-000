dcfffcf381f1fbff1690ff5c369016ab80b8dcbe
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-09 13:24:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var katzDeli = []
 
+function currentLine (line){
+  if (!line.length);
+  return "The line is currently empty."
+}
+
 var katzDeliLine = []
 
 function takeANumber (katzDeliLine, name){
